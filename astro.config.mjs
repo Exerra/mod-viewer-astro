bacd81cb2743b5c19df4540904a5979b3e2d6076
Exerra <exerra@exerra.xyz>
2023-01-22 21:26:23 +0200

    basically entire website done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
 import { defineConfig } from 'astro/config';
-
+import storyblok from "@storyblok/astro";
+import { loadEnv } from 'vite';
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        Mod: "storyblok/mod",
+        Page: "storyblok/Page",
+        "fnv-mods": "storyblok/lists/fnv",
+        "morrowind-mods": "storyblok/lists/morrowind"
+      }
+    })
+  ]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"theme-100": "#FCF7F0",
+				"theme-200": "#F9EFE2",
+				"theme-500": "#D5933A"
+			}
+		},
 	},
 	plugins: [],
 }
