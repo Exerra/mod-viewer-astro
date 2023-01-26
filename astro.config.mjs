f32f1b343b760bf7d4903ec5b78ca2fb2c338807
Exerra <exerra@exerra.xyz>
2023-01-26 20:12:11 +0200

    slight improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,20 @@ import storyblok from "@storyblok/astro";
 import { loadEnv } from 'vite';
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
+import image from "@astrojs/image";
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        Mod: "storyblok/mod",
-        Page: "storyblok/Page",
-        "fnv-mods": "storyblok/lists/fnv",
-        "morrowind-mods": "storyblok/lists/morrowind"
-      }
-    })
-  ]
+  integrations: [tailwind(), storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      Mod: "storyblok/mod",
+      Page: "storyblok/Page",
+      "fnv-mods": "storyblok/lists/fnv",
+      "morrowind-mods": "storyblok/lists/morrowind"
+    }
+  }), image()]
 });
